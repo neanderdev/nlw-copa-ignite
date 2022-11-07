da3b9b039c18e982a99810bbe913beea2ea476d7
neanderdev <neander.faria@hotmail.com>
2022-11-07 07:54:22 -0300

    NLW Finalizada

---
+++ mobile/src/components/Guesses.tsx
@@ -0,0 +1,106 @@
+import { useEffect, useState } from 'react';
+import { Share } from 'react-native';
+import { FlatList, useToast } from 'native-base';
+
+import { api } from '../services/api';
+
+import { Loading } from './Loading';
+import { Game, GameProps } from '../components/Game';
+import { EmptyMyPoolList } from '../components/EmptyMyPoolList';
+
+interface Props {
+  poolId: string;
+  code: string;
+}
+
+export function Guesses({ poolId, code }: Props) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [games, setGames] = useState<GameProps[]>([]);
+  const [firstTeamPoints, setFirstTeamPoints] = useState('');
+  const [secondTeamPoints, setSecondTeamPoints] = useState('');
+
+  const toast = useToast();
+
+  async function fetchGames() {
+    try {
+      setIsLoading(true);
+
+      const response = await api.get(`/pools/${poolId}/games`);
+      setGames(response.data.games);
+    } catch (error) {
+
+      toast.show({
+        title: 'Não foi possível listar os jogos',
+        placement: 'top',
+        bgColor: 'red.500'
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function handleGuessConfirm(gameId: string) {
+    try {
+      if (!firstTeamPoints || !secondTeamPoints) {
+        return toast.show({
+          title: 'Informe o placar para palpitar',
+          placement: 'top',
+          bgColor: 'red.500'
+        });
+      }
+
+      await api.post(`/pools/${poolId}/games/${gameId}/guesses`, {
+        firstTeamPoins: Number(firstTeamPoints),
+        secondTeamPoins: Number(secondTeamPoints),
+      });
+
+      toast.show({
+        title: 'Palpite realizado com sucesso',
+        placement: 'top',
+        bgColor: 'green.500'
+      });
+
+      fetchGames();
+
+    } catch (error) {
+      console.log(error);
+
+      toast.show({
+        title: 'Não foi possível enviar o palpite',
+        placement: 'top',
+        bgColor: 'red.500'
+      });
+    }
+  }
+
+  async function handleCodeShare() {
+    await Share.share({
+      message: code,
+    });
+  }
+
+  useEffect(() => {
+    fetchGames();
+  }, [poolId]);
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  return (
+    <FlatList
+      data={games}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) => (
+        <Game
+          data={item}
+          setFirstTeamPoints={setFirstTeamPoints}
+          setSecondTeamPoints={setSecondTeamPoints}
+          onGuessConfirm={() => handleGuessConfirm(item.id)}
+        />
+      )}
+      ListEmptyComponent={<EmptyMyPoolList code={code} onShare={handleCodeShare} />}
+      _contentContainerStyle={{ pb: 10 }}
+    />
+  );
+}
---
+++ server/prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Neander de Souza",
+      email: "neander.faria@hotmail.com",
+      avatarUrl: "https://github.com/neanderdev.png",
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Example Pool",
+      code: "BOL123",
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-07T18:00:00.782Z",
+      firstTeamCountryCode: "BR",
+      secondTeamCountryCode: "DE",
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-07T16:00:00.782Z",
+      firstTeamCountryCode: "AR",
+      secondTeamCountryCode: "BE",
+
+      guesses: {
+        create: {
+          firstTeamPoins: 2,
+          secondTeamPoins: 1,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main();
